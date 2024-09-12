94f9f4471e626c4ebe95f1bb1ebb3c91949b39f9
himonhimu <kmhimu87@gmail.com>
2024-09-12 15:30:10 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js v 1-2-1</code>.
+            Get started by editing <code>src/app/page.js v 1-2-1 test</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
