bd5cac97b779a93e664a0a14a4370649e84bf0a1
himonhimu <kmhimu87@gmail.com>
2024-09-12 16:24:24 +0600

    pm2 start

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Pm2 restart work (1)</h1>
+        <h3>Pm2 start with user change from root to himon</h3>
 
         <div className={styles.ctas}>
           <a
