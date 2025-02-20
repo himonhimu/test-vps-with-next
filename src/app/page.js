f3bfc5b3b93f7d22ab45bcc158cce7ac3c335c9f
himonhimu <kmhimu87@gmail.com>
2025-02-20 16:41:34 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ export default function Home() {
       <main className={styles.main}>
         <h1>Pm2 restart work (1)</h1>
         <h1>This page is deployed with Jenkins</h1>
+        <h1>This page is deployed with Jenkins (1)</h1>
+        {/* <h1>This page is deployed with Jenkins</h1> */}
         <h3>Pm2 start with user change from root to himon</h3>
         <h3>Test working</h3>
       </main>
